3de551fccdd7e5fe405bd384b9ba487ce4b5482b
Taiwo Adepoju <tadepoju95@gmail.com>
2020-08-09 04:06:09 -0500

    Completed the objects problem set.

--- objects.js
+++ objects.js
@@ -34,3 +34,39 @@ let returnLength = () => {
     return getLengthOfObject;
 }
 console.log(returnLength());
+
+//function that checks whether an object contains the specified key 
+
+let person = {
+  firstName: 'Loveth',
+  lastName: 'King',
+  age: 29
+};
+
+let objectContainsKey = () => {
+  let checksIfObjectHasKey = person.hasOwnProperty('age');
+    return checksIfObjectHasKey;
+}
+console.log(objectContainsKey());
+
+//write a function to sort the array of objects by title value 
+
+let books = [
+   { author: 'Bill Gates', title: 'The Road Ahead', libraryID: 1254},
+   { author: 'Steve Jobs', title: 'Walter Isaacson', libraryID: 4264},
+   { author: 'Suzanne Collins', title: 'Mockingjay: The Final Book of The Hunger Games', libraryID: 3245}
+]
+
+let sortArray = () => {
+  let sortByTitleValue = books.sort((a, b) => {
+    if(a.title < b.title) {
+      return -1;
+    }
+    if(a.title > b.title) {
+      return 1;
+    }
+    return 0;
+  });
+  return sortByTitleValue;
+}
+console.log(sortArray());
