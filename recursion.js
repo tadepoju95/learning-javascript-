1e1cb8cdd0530dae3c21173a4a60ea9ee150aaa4
Taiwo Adepoju <tadepoju95@gmail.com>
2020-08-19 02:00:38 -0500

    Fixed my recursion solutions.

--- recursion.js
+++ recursion.js
@@ -1,21 +1,23 @@
 //function to calculate the factorial of a number
 
 let calculateFactorialOfN = n => {
-  let getFactorial = 1;
-  for(let i = n; i >= 1; i--) {
-    getFactorial = getFactorial * i;
+  if(n < 0) {
+    return;
   }
-  return getFactorial;
+  if(n === 0) {
+    return 1;
+  }
+  return n * calculateFactorialOfN(n - 1);
 }
-console.log(calculateFactorialOfN(6));
+console.log(calculateFactorialOfN(4));
 
 //function to compute the sum of an array of integers
 
 let findSumOfNumbers = arrayOfIntegers => {
-  let addingAllNumbers = arrayOfIntegers.reduce((accumulator, currentValue) => {
-    return accumulator + currentValue;
-  });
-return addingAllNumbers;
+  if(arrayOfIntegers.length === 0) {
+    return 0;
+  }
+  return arrayOfIntegers[0] + findSumOfNumbers(arrayOfIntegers.slice(1));
 }
-console.log(findSumOfNumbers([1, 3, 25, 6, 9]));
+console.log(findSumOfNumbers([1, 3, 25, 6, 9])); 
 
